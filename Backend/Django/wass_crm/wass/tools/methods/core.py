853576bd809dffac2f7056733cf303e95c1b1069
Chris <mavroboticswebdev7690@gmail.com>
2024-11-08 14:20:16 -0500

    Create Env File

--- Backend/Django/wass_crm/wass/tools/methods/core.py
+++ Backend/Django/wass_crm/wass/tools/methods/core.py
@@ -4,7 +4,7 @@ import json
 
 def get_env_file(secret_name):
     try:
-        with open("core/core_env.json") as f:
+        with open("core/core_env_v.json") as f:
             secret_file = json.loads(f.read())
         return secret_file[secret_name]
     except:
