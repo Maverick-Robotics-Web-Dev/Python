45aa6229d5a4a47c2700c9259918a22f24ef2269
Maverick <mavroboticswebdev7690@gmail.com>
2024-12-08 23:42:14 -0500

    Edit Model

--- Backend/Django/wass_crm/wass/core/urls.py
+++ Backend/Django/wass_crm/wass/core/urls.py
@@ -16,9 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('', include('docs.urls'), name='docs'),
     path('admin/', admin.site.urls, name='admin'),
     path('wass/api/v1/', include('routes.urls'), name='routes'),
 ]
+
+urlpatterns+=static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
